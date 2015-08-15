bc3fb91311e4f26c6f3efd006a807e7462247754
Jeff Triplett <jeff.triplett@gmail.com>
2015-08-15 15:41:22 -0500

    Refactor to work with setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='itermocil',
+    version='0.1.5',
+    url='https://github.com/TomAnthony/itermocil',
+    license='MIT',
+    description='Create pre-defined window/pane layouts and run commands in iTerm',
+    author='Tom Anthony',
+    author_email='',
+    packages=find_packages(),
+    py_modules=['itermocil'],
+    package_data={},
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+            'itermocil = itermocil:main',
+        ]
+    },
+    install_requires=[
+        'PyYAML',
+    ]
+)

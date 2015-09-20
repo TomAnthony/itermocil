8c03ba2742af457391207bb7331880bfc7ab6509
Tom Anthony <git@tomanthony.co.uk>
2015-09-20 14:33:14 +0100

    Bump version number -> 0.1.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='itermocil',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/TomAnthony/itermocil',
     license='MIT',
     description='Create pre-defined window/pane layouts and run commands in iTerm',

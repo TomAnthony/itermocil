d639a8e5c66e7cd6bb9fcf5dce81be8fec79dfe8
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-22 13:59:20 +0800

    update iTermocil hardcoded versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='itermocil',
-    version='1.0.0',
+    version='1.0.3',
     url='https://github.com/TomAnthony/itermocil',
     license='MIT',
     description='Create pre-defined window/pane layouts and run commands in iTerm',
